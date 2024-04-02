68d9fed7ca108dc265bc49e0ac5e20720ec078f4
kota-aso <kotaaso1212@gmail.com>
2024-04-02 23:57:24 +0900

    ボタンをオブジェクト化

--- Stopwatch/ContentView.swift
+++ Stopwatch/ContentView.swift
@@ -15,66 +15,55 @@ struct ContentView: View {
         VStack {
             Text(String(format: "%.1f", secondsElapsed))
                 .font(Font(UIFont.monospacedDigitSystemFont(ofSize: 120, weight: .black)))
-            Button {
-                start()
-            } label: {
-                Text("start")
-                    .font(.largeTitle)
-                    .frame(width: 200)
-                    .foregroundColor(.white)
-                    .padding()
-                    .background(.green)
-                    .cornerRadius(10)
-            }
-            Button {
-                stop()
-            } label: {
-                Text("stop")
-                    .font(.largeTitle)
-                    .frame(width: 200)
-                    .foregroundColor(.white)
-                    .padding()
-                    .background(.red)
-                    .cornerRadius(10)
-            }
-            Button {
-                reset()
-            } label: {
-                Text("reset")
-                    .font(.largeTitle)
-                    .frame(width: 200)
-                    .foregroundColor(.white)
-                    .padding()
-                    .background(.gray)
-                    .cornerRadius(10)
-            }
-
+            ButtonView(buttonAction: { start() }, text: "Start", color: .green)
+            ButtonView(buttonAction: { stop() }, text: "Stop", color: .red)
+            ButtonView(buttonAction: { reset() }, text: "Reset", color: .gray)
         }
         .padding()
     }
+    
     func start() {
         if timer == nil {
-        timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { _ in
-            secondsElapsed += 0.1
-        }
+            timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { _ in
+                secondsElapsed += 0.1
+            }
         }
     }
-
+    
     func stop() {
         if let time = timer {
-        time.invalidate()
+            time.invalidate()
         }
         timer = nil
     }
-
+    
     func reset() {
         if let time = timer {
-        time.invalidate()
+            time.invalidate()
         }
         timer = nil
         secondsElapsed = 0
     }
+}
 
+struct ButtonView: View {
+    let buttonAction: () -> Void
+    let text: String
+    let color: Color
+    
+    var body: some View {
+        Button {
+            buttonAction()
+        } label: {
+            Text(text)
+                .font(.largeTitle)
+                .frame(width: 200)
+                .foregroundColor(.white)
+                .padding()
+                .background(color)
+                .cornerRadius(10)
+        }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
