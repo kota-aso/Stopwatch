74b4c5250161c3bafda6dc04f43f4c759db5467b
kota-aso <kotaaso1212@gmail.com>
2024-04-02 23:05:05 +0900

    Initial Commit

---
+++ Stopwatch/StopwatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopwatchApp.swift
+//  Stopwatch
+//
+//  Created by Kota Aso on 2024/04/02.
+//
+
+import SwiftUI
+
+@main
+struct StopwatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
